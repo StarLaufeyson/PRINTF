65f44edd1f463026f40763c2936bf0a38ab28baa
Estrella Luno Lago <eluno-la@c4r7s3.42urduliz.com>
2023-07-13 17:56:43 +0200

    .h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: eluno-la <eluno-la@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 17:58:27 by eluno-la          #+#    #+#             */
-/*   Updated: 2023/06/29 17:58:29 by eluno-la         ###   ########.fr       */
+/*   Updated: 2023/07/13 17:54:29 by eluno-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,9 @@
 
 size_t	ft_strlen(const char *str);
 int		ft_printchar(char c);
+int		ft_print_integer(int num);
+int		ft_print_hex(unsigned long long num, char c);
+int		ft_printargs(va_list args, const char format);
+int		ft_print_integer_unsig(unsigned int num);
 
 #endif  // FT_PRINT_F
