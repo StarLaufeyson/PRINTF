4fe200ff701126d78c0c62fc1a08e32f0be29ec0
Estrella Luno Lago <eluno-la@c4r5s3.42urduliz.com>
2023-07-14 19:09:46 +0200

    printstr

--- ft_print_str.c
+++ ft_print_str.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_str.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eluno-la <eluno-la@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/14 18:52:11 by eluno-la          #+#    #+#             */
+/*   Updated: 2023/07/14 18:54:00 by eluno-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include	"ft_printf.h"
 
 int	ft_print_str(char *str)
@@ -17,7 +29,10 @@ int	ft_print_str(char *str)
 
 int	main(void)
 {
-	char *sentence = "I'm Klaus Mikaelson!";
-	int	length = ft_print_str(sentence);
+	char	*sentence;
+	int		length;
+
+	sentence = "I am Klaus Mikaelson!";
+	length = ft_print_str(sentence);
 	return (0);
 }
